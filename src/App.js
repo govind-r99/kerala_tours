9465ca34fa2180a92b8b371e91f9791207c570e7
govind-r99 <govindrajanbabu99@gmail.com>
2023-08-09 14:56:43 +0530

    user finish

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/enquiry' element={<Enquiry />}></Route>
